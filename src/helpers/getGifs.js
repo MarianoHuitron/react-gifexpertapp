6543c283ac6b5659c2d08a4f88df1a9cdcf51c7a
Mariano <mariano.14th@gmail.com>
2020-09-06 13:31:31 -0600

    https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async ( category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=eK73le5Z4t2BJNJnDHFwR02GqsrT3Uro`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=eK73le5Z4t2BJNJnDHFwR02GqsrT3Uro`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
