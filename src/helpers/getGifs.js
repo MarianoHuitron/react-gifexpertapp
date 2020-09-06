430537e2ee9f21d0929a1defdace0a8395f8623f
Mariano <mariano.14th@gmail.com>
2020-09-06 13:10:52 -0600

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+export const getGifs = async ( category ) => {
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=eK73le5Z4t2BJNJnDHFwR02GqsrT3Uro`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    
+    return gifs;
+}
