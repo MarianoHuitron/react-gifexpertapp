1482507583a6cf9dece2b40088da60daab60f64a
Mariano <mariano.14th@gmail.com>
2020-09-06 13:12:44 -0600

    docs agregado

---
+++ docs/precache-manifest.87f2aa0d192b4385d71c63f6b889f645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac14b9493d3eaa7e46b93a30180f223",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82e629f897a48155901",
+    "url": "/static/css/main.0c640f5e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82e629f897a48155901",
+    "url": "/static/js/main.26bc84a0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
